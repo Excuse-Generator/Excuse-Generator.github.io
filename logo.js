8a48488d725636fea4223537410d46223838089a
GrahamSH-LLK <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-07-03 20:35:51 -0400

    Update logo.js

--- logo.js
+++ logo.js
@@ -28,8 +28,8 @@
     });
     _assign(outerMarquee.style, {
       position: "fixed",
-      width: "auto",
-      height: "auto",
+      width: 10,
+      height: 10,
       top: 0,
       left: 0,
       zIndex: 999999,
